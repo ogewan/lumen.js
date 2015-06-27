f66127e120c0608231347fcf35c1b015dda1540f
Seun Ogedengbe <seun40@gmail.com>
2015-06-27 11:07:54 -0500

    added lumen.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # lumen.js
 Dynamic CSS coloring based on locale.
+![Sunlight Cycle](http://pixsylated.com/blog/pix/Daylight-Cycle-Sunlight.jpg)
--- lumen.js
+++ lumen.js
@@ -1,10 +1,12 @@
-function dJSAO() {
- var element = document.createElement("script");
- element.src = "defer.js";
- document.body.appendChild(element);
- }
- if (window.addEventListener)
- window.addEventListener("load", dJSAO, false);
- else if (window.attachEvent)
- window.attachEvent("onload", dJSAO);
- else window.onload = downloadJSAtOnload;
+/*! lumen.js (c) 2015 Oluwaseun Ogedengbe, MIT seun40.github.io/lumen.js/*/
+var rng = [[0,0,0],[0,0,0]];
+function lmn(){
+    var day = Math.floor(Date.now()/1000)%86400,lt=[Math.floor(day/255),Math.floor(day/255)%255,Math.floor(day/255)],dk=[255-Math.floor(day/255),255-Math.floor(day/255),255-Math.floor(day/255)];
+    //var lt=[day%255,day%255,day%255],dk=[255-(day%255),255-(day%255),255-(day%255)];
+    document.body.setAttribute("style","background-color: rgb("+lt[0]+","+lt[1]+","+lt[2]+");color: rgb("+dk[0]+","+dk[1]+","+dk[2]+");");
+    console.log("rgb("+lt[0]+","+lt[1]+","+lt[2]+")","rgb("+dk[0]+","+dk[1]+","+dk[2]+")");
+    setTimeout(lmn,100);
+} 
+if (window.addEventListener) window.addEventListener("load", lmn, false);
+else if (window.attachEvent) window.attachEvent("onload", lmn);
+else window.onload = lmn;
